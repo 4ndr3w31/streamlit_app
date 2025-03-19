c5b0ca5660af083f4c2bdb0f6b8e30d70ae864b0
4ndr3w31 <4ndr3w.widyanata31@gmail.com>
2025-03-19 16:42:33 +0700

    Update Streamlit_code.py

--- Streamlit_code.py
+++ Streamlit_code.py
@@ -24,13 +24,13 @@ def main():
         result = make_prediction(features)
         st.success(f'The prediction is: {result}')
         
-    def make_prediction(features):
+def make_prediction(features):
         #use the loaded model to make predictions
         input_array = np.array(features).reshape(1,-1)
         prediction = model.predict(input_array)
         return prediction[0]
-    
-    if _name_ == '_main_':
+        
+if _name_ == '_main_':
         main()
 
 
